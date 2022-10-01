035f62c75fbe6ac88adfd92cc58f03da49f4cdaa
ishida <ishidas1155@gmail.com>
2022-10-01 14:49:37 +0900

    fixed filename

---
+++ 00/action.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>練習問題</title>
+</head>
+<body>
+	<div class="container">
+	<?php ?>
+		
+	</div>
+</body>
+</html>
